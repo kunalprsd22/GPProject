89141c19b4be4657c0797c35730be265fbf7dd40
Kunal Prasad <>
2026-01-06 23:14:07 +0530

    Add the text animation in controller

--- GPProject/ViewController.swift
+++ GPProject/ViewController.swift
@@ -8,14 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myName: UILabel!
+    
+    private var textToAnimate = ""
+    private var currentIndex = 0
+    private var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // My second commit
-        // git stash message
-        // my second tag v2
+        animateText("")
     }
-
-
+    
+    func animateText(_ text: String) {
+        myName.text = ""
+        textToAnimate = text
+        currentIndex = 0
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: 0.2,
+            target: self,
+            selector: #selector(addNextCharacter),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc private func addNextCharacter() {
+        guard currentIndex < textToAnimate.count else {
+            timer?.invalidate()
+            return
+        }
+        
+        let index = textToAnimate.index(
+            textToAnimate.startIndex,
+            offsetBy: currentIndex
+        )
+        myName.text?.append(textToAnimate[index])
+        currentIndex += 1
+    }
 }
 
