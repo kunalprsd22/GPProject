fe26afc421748f8422b0faf3c0c387856c50bc24
Kunal Prasad <>
2026-01-07 23:14:49 +0530

    Remove function

--- GPProject/ViewController.swift
+++ GPProject/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        animateText("Text Data")
     }
     
     func animateText(_ text: String) {
