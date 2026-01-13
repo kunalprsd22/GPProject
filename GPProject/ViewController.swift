d5ecaee98c6eaa23649fe60e5e60643bceef7d1c
Kunal Prasad <>
2026-01-13 22:53:47 +0530

    Add function `animateText`

--- GPProject/ViewController.swift
+++ GPProject/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        animateText("hello")
     }
     
     func animateText(_ text: String) {
